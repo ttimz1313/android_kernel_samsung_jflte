268e858e3873643bec8eee57abcaa2e8629f5bff
Steve Kondik <shade@chemlab.org>
2013-07-26 01:10:39 -0700

    Fix uninitialized variable warnings with gcc-4.7
    Change-Id: Ia29ef35dc710721ec15d5e2cf849e027ae0e7724

--- net/netfilter/xt_socket.c
+++ net/netfilter/xt_socket.c
@@ -276,7 +276,7 @@ xt_socket_get6_sk(const struct sk_buff *skb, struct xt_action_param *par)
 	struct ipv6hdr ipv6_var, *iph = ipv6_hdr(skb);
 	struct udphdr _hdr, *hp = NULL;
 	struct sock *sk;
-	const struct in6_addr *daddr = NULL, *saddr = NULL;
+	const struct in6_addr *daddr = 0, *saddr = 0;
 	__be16 dport = 0, sport = 0;
 	int thoff, tproto;
 
